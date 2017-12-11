893aadda154e74aff30d1d54c5d0a3226dab5164
yatishhr <yatishhr@gmail.com>
2017-12-11 16:05:15 +0530

    .

--- q7.py
+++ q7.py
@@ -11,6 +11,7 @@ def home():
 def fname():
     text1="Name :" + str(request.form["Name"])
     text2="Age :" + str(request.form["Age"])
-    print(text1+"\n"+text2, file=sys.stderr)
-    return ""
+    print(text1+"\n"+text2, file=sys.stdout)
+    sys.stdout.flush()
+    return "Check the Console"
 app.run(port=8080)
